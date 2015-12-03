ae89f2aff659c38fa2ecb72243b760ff42bcfad8
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-12-03 11:58:38 -0430

    beautify config source file

--- snaplayer/config.py
+++ snaplayer/config.py
@@ -1,10 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+snaplayer.config
+~~~~~~~~
+
+Configuration file implementation
+
+:copyright: (c) 2015 by Alejandro Ricoveri
+:license: MIT, see LICENSE for more details.
+"""
+
 import yaml
+from schema import Schema, Optional
 
 class Config():
+    """Configuration file
 
-    """Docstring for . """
+    Its role's very simple, to load
+    a YAML file, validate it and from there
+    set options to be used by this program
+    """
 
     def __init__(self, *, config_file):
+        """Constructor
+
+        :param config_file: path to configuration file
+        """
+
+        # Default set of options
         self._options = {
             'hourly': True,
             'monthly': True,
@@ -19,7 +42,8 @@ class Config():
             'public_ip': None,
             'private_ip': None
             }
-        #
+
+        # Validation schema for each option
         self._schema = Schema({
             Optional('hourly'): bool,
             Optional('monthly'): bool,
@@ -34,10 +58,13 @@ class Config():
             Optional('public_ip'): str,
             Optional('private_ip'): str
             })
-        #
+
+        # Load YAML file and validate it
         with open(config_file, "r") as file:
             data = self._schema.validate(yaml.load(file))
             for key, value in data.items():
+                # overwrite defaults with values taken
+                # from the configuration file
                 if key in self._options:
                     self._options[key] = value
 
