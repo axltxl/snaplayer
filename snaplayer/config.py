7e1b09a3e7f9a632d9d89e3c10212ed06aae6aac
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-12-03 11:27:24 -0430

    all data coming from config file is properly validated thanks to Schema

--- snaplayer/config.py
+++ snaplayer/config.py
@@ -6,22 +6,37 @@ class Config():
 
     def __init__(self, *, config_file):
         self._options = {
-                'hourly': True,
-                'monthly': True,
-                'tags': None,
-                'cpus': None,
-                'memory': None,
-                'hostname': None,
-                'domain': None,
-                'local_disk': None,
-                'datacenter': None,
-                'nic_speed': None,
-                'public_ip': None,
-                'private_ip': None
-                }
+            'hourly': True,
+            'monthly': True,
+            'tags': None,
+            'cpus': None,
+            'memory': None,
+            'hostname': None,
+            'domain': None,
+            'local_disk': None,
+            'datacenter': None,
+            'nic_speed': None,
+            'public_ip': None,
+            'private_ip': None
+            }
+        #
+        self._schema = Schema({
+            Optional('hourly'): bool,
+            Optional('monthly'): bool,
+            Optional('tags'): lambda tags: type(tags) is list and all([type(t) is str for t in tags]),
+            Optional('cpus'): int,
+            Optional('memory'): int,
+            Optional('hostname'): str,
+            Optional('domain'): str,
+            Optional('local_disk'): str,
+            Optional('datacenter'): str,
+            Optional('nic_speed'): int,
+            Optional('public_ip'): str,
+            Optional('private_ip'): str
+            })
         #
         with open(config_file, "r") as file:
-            data = yaml.load(file)
+            data = self._schema.validate(yaml.load(file))
             for key, value in data.items():
                 if key in self._options:
                     self._options[key] = value
