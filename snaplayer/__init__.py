d98c8115ef31b70a8e09e00c853792f788fa26a9
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2015-12-04 09:45:13 -0430

    a better module init file

--- snaplayer/__init__.py
+++ snaplayer/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-PKG_NAME = 'snaplayer'
-PKG_URL  = 'http://github.com/axltxl/snaplayer'
-
+PKG_URL = 'https://github.com/axltxl/snaplayer'
+__name__ = 'snaplayer'
+__author__ = 'Alejandro Ricoveri'
 __version__ = '0.0.1'
+__licence__ = 'MIT'
+__copyright__ = 'Copyright (c) Alejandro Ricoveri'
